9ecce2df028923097d13277a7a48318e4bb7f206
Ito Shen <itoshen@Itos-MacBook-Pro-13-inch.local>
2018-08-15 17:40:35 +0800

    initial commit

---
+++ songSort_NeteaseMusic.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# cd currentDir
+target_dir=`pwd`
+cd $target_dir
+echo "current_dir:" $target_dir
+
+# mkdir
+for file in *
+do
+
+    if [ ! -d "$file" ]
+    then
+        # Top tear folder name
+        artist_name=$(echo $(basename $file) | sed 's/\.[^.]*$//' | cut -d '-' -f1)
+        
+        # # Secondary folder name
+        # album_name=$(basename $var) | sed 's/\.[^.]*$//' | cut -d '-' -f2
+
+
+        if [ ! -e "$target_dir/$artist_name" ]
+        then
+            mkdir "$artist_name"
+            echo "make new dir: $artist_name"
+        fi
+        
+        # if [ ! -d "$target_dir/$artist_name/$album_name" ]; then
+        #     mkdir "$target_dir/$artist_name/$album_name"
+        #     echo "starting new album: $album_name"
+        # fi
+
+        echo "moving file: $file"
+        mv "$target_dir/$file"  "$target_dir/$artist_name/$file"
+
+    fi
+    
+done
+
