014cde530d9fa2ce9e6cf802795297051e936df5
ItoShen <itoshen@Itos-MacBook-Pro-15-inch.local>
2018-08-18 06:01:26 +0800

    codes update

--- music_sorting_NeteaseMusic.sh
+++ music_sorting_NeteaseMusic.sh
@@ -9,8 +9,7 @@ echo "current_dir:" $target_dir
 for file in *
 do
 
-    if [ ! -d "$file" ];
-    then
+    if [ ! -d "$file" ];then
         # Top tear folder name
         artist_name=$(echo $(basename $file) | sed 's/\.[^.]*$//' | cut -d '-' -f1)
         
@@ -18,13 +17,12 @@ do
         # album_name=$(basename $var) | sed 's/\.[^.]*$//' | cut -d '-' -f2
 
 
-        if [ ! -e "$target_dir/$artist_name" ];
-        then
+        if [ ! -e "$target_dir/$artist_name" ];then
             mkdir "$artist_name"
             echo "make new dir: $artist_name"
         fi
         
-        # if [ ! -d "$target_dir/$artist_name/$album_name" ]; then
+        # if [ ! -e "$target_dir/$artist_name/$album_name" ]; then
         #     mkdir "$target_dir/$artist_name/$album_name"
         #     echo "starting new album: $album_name"
         # fi
