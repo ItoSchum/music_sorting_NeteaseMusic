1b67883330baa2c1a8a940da377b11ca34a7a13e
ItoShen <itoshen@Itos-MacBook-Pro-15-inch.local>
2018-08-18 05:23:42 +0800

    v1.1 Update & README Update

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## Usage
 1. Change your Teriminal's current directory to `[NeteaseMusic_downloads_dir]`.
 2. Run the script.
+
+## Update
+- v1.0 Initial Ver.
+- v1.1 When some songs are already sorted to folders named with the artist name, it won't create a same-name folder again.
--- music_sorting_NeteaseMusic.sh
+++ music_sorting_NeteaseMusic.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# cd currentDir
+# cd target_dir
 target_dir=`pwd`
 cd $target_dir
 echo "current_dir:" $target_dir
 
-# mkdir
+# mkdir & mv file
 for file in *
 do
 
-    if [ ! -d "$file" ]
+    if [ ! -d "$file" ];
     then
         # Top tear folder name
         artist_name=$(echo $(basename $file) | sed 's/\.[^.]*$//' | cut -d '-' -f1)
@@ -18,7 +18,7 @@ do
         # album_name=$(basename $var) | sed 's/\.[^.]*$//' | cut -d '-' -f2
 
 
-        if [ ! -e "$target_dir/$artist_name" ]
+        if [ ! -e "$target_dir/$artist_name" ];
         then
             mkdir "$artist_name"
             echo "make new dir: $artist_name"
@@ -28,11 +28,10 @@ do
         #     mkdir "$target_dir/$artist_name/$album_name"
         #     echo "starting new album: $album_name"
         # fi
-
-        echo "moving file: $file"
-        mv "$target_dir/$file"  "$target_dir/$artist_name/$file"
-
     fi
+
+    echo "moving file: $file"
+    mv "$target_dir/$file"  "$target_dir/$artist_name/$file"
     
 done
 
